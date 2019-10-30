3a2cf0b8abbe45d32ce994e658355fafabd86f11
asadnaeem1 <asadnaeem12345@gmail.com>
2019-10-31 00:17:52 +0500

    PolyAlphabetic Cipher Implemented

---
+++ PolyAlphabeticCipher/PolyAlphabeticCipher/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyAlphabeticCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PolyAlphabeticCipher\n1. Encrypt\n2. Decrypt");
+            int choice = int.Parse(Console.ReadLine());
+            string[] keys = new string[]{
+                "asdfghjklpoiuytrewqzxcvbnm",
+                "qazwsxedcrfvtgbyhnupjmikol",
+                "zxcvbnmkioplujhytgfredswqa"
+            };
+            Console.Write("Input: ");
+            string input = Console.ReadLine().ToLower();
+            string output = "";
+            int n = 0;
+            switch (choice)
+            {
+                case 1:
+                    foreach (char c in input)
+                    {
+                        if (Char.IsLetter(c))
+                        {
+                            output += keys[n][(int)c - 97];
+                        }
+                        else
+                        {
+                            output += c;
+                        }
+                        n++;
+                        n %= 3;
+                    }
+                    break;
+                case 2:
+                    foreach (char c in input)
+                    {
+                        if (Char.IsLetter(c))
+                        {
+                            output += (char)(keys[n].IndexOf(c) + 97);
+                        }
+                        else
+                        {
+                            output += c;
+                        }
+                        n++;
+                        n %= 3;
+                    }
+                    break;
+            }
+
+            Console.WriteLine("Output: " + output);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ PolyAlphabeticCipher/README.md
@@ -0,0 +1,11 @@
+# PolyAlphabetic Cipher
+## BSCS-631 Network Security & Cryptography
+Implementation of PolyAlphabetic Cipher.
+
+Language: C#
+
+A polyalphabetic cipher is cipher based on substitution, using multiple substitution alphabets.
+
+Name: Muhammad Asad
+
+Roll No. B16158051
