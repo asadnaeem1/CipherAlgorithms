ef9c75782295bffeb0f5a64494623eec7d676fd2
asadnaeem1 <asadnaeem12345@gmail.com>
2019-11-02 20:12:19 +0500

    Rail Fence Cipher Implemented

---
+++ RailFenceCipher/README.md
@@ -0,0 +1,11 @@
+# Rail Fence Cipher
+## BSCS-631 Network Security & Cryptography
+Implementation of Rail Fence Cipher.
+
+Language: C#
+
+The Rail Fence cipher (sometimes called zigzag cipher) is a transposition cipher that jumbles up the order of the letters of a message using a basic algorithm.
+
+Name: Muhammad Asad
+
+Roll No. B16158051
---
+++ RailFenceCipher/RailFenceCipher/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailFenceCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Playfair Cipher\n1. Encrypt\n2. Decrypt");
+            int choice = int.Parse(Console.ReadLine());
+            Console.Write("Depth: ");
+            int depth = int.Parse(Console.ReadLine());
+            Console.Write("Input: ");
+            string input = new string(Console.ReadLine().ToLower().ToCharArray().Where(c => !Char.IsWhiteSpace(c)).ToArray());
+            string[] depthList = new string[depth];
+            for (int i = 0; i < depth; i++)
+            {
+                depthList[i] = "";
+            }
+            string output = "";
+            int n = 0;
+            bool forward = true;
+            //encryption
+            foreach (char c in input)
+            {
+                if (forward && n == depth)
+                {
+                    n -= 2;
+                    forward = false;
+                }
+                else if (n < 0)
+                {
+                    n += 2;
+                    forward = true;
+                }
+                if (forward)
+                {
+                    depthList[n] += c;
+                    n++;
+                }
+                else
+                {
+                    depthList[n] += c;
+                    n--;
+                }
+            }
+            switch (choice)
+            {
+                //Ecryption
+                case 1:
+                    foreach (string s in depthList)
+                    {
+                        output += s;
+                    }
+                    break;
+
+                //Decryption
+                case 2:
+                    int temp = 0;
+                    for (int i = 0; i < depth; i++)
+                    {
+                        string sTemp = "";
+                        for (int j = 0; j < depthList[i].Length; j++)
+                        {
+                            sTemp += input[temp];
+                            temp++;
+                        }
+                        depthList[i] = sTemp;
+                    }
+
+                    n = 0;
+                    forward = true;
+                    int[] count = new int[depth];
+                    for (int i = 0; i < depth; i++)
+                    {
+                        count[i] = 0;
+                    }
+                    foreach (char c in input)
+                    {
+                        if (forward && n == depth)
+                        {
+                            n -= 2;
+                            forward = false;
+                        }
+                        else if (n < 0)
+                        {
+                            n += 2;
+                            forward = true;
+                        }
+                        output += depthList[n][count[n]];
+                        count[n]++;
+                        if (forward)
+                        {
+                            n++;
+                        }
+                        else
+                        {
+                            n--;
+                        }
+                    }
+                    break;
+            }
+            Console.WriteLine("Output: " + output);
+            Console.ReadKey();
+        }
+    }
+}
