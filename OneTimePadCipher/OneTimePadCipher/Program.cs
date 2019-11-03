398851680dbd166287a47de91bd186a768fc1bbc
asadnaeem1 <asadnaeem12345@gmail.com>
2019-11-03 16:06:27 +0500

    One Time Pad Cipher Implemented

---
+++ OneTimePadCipher/OneTimePadCipher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneTimePadCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("One Time Pad Cipher\n1. Encrypt\n2. Decrypt");
+            int choice = int.Parse(Console.ReadLine());
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+            Console.Write("Key: ");
+            string key = Console.ReadLine();
+            if (input.Length != key.Length)
+            {
+                Console.Clear();
+                Console.WriteLine("Input and Key should be of equal length");
+                goto start;
+            }
+            string output = "";
+            
+            switch (choice)
+            {
+                //Ecryption
+                case 1:
+                //Decryption
+                case 2:
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        output += Convert.ToInt16((input[i] == '1') ^ (key[i] == '1'));
+                    }
+                        break;
+            }
+            Console.WriteLine("Output: " + output);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OneTimePadCipher/README.md
@@ -0,0 +1,13 @@
+# One Time Pad Cipher
+## BSCS-631 Network Security & Cryptography
+Implementation of One Time Pad Cipher.
+
+Language: C#
+
+The Input to the program should be binary.
+
+In cryptography, the one-time pad (OTP) is an encryption technique that cannot be cracked, but requires the use of a one-time pre-shared key the same size as, or longer than, the message being sent. In this technique, a plaintext is paired with a random secret key (also referred to as a one-time pad). Then, each bit or character of the plaintext is encrypted by combining it with the corresponding bit or character from the pad using modular addition.
+
+Name: Muhammad Asad
+
+Roll No. B16158051
